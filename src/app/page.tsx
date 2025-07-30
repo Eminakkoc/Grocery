24f67e460fb30b5d2baf5f2c82bf6184332da55f
Emin Akkoc <emin.akkoc@gmail.com>
2025-07-30 16:41:32 +0300

    fix: page props
    - Fix page search params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import Sidebar from '@/components/Sidebar';
 export default async function Page({
     searchParams,
 }: {
-    searchParams?: { [key: string]: string | string[] | undefined };
+    searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
+    const params = await searchParams;
     return (
         <div className="flex gap-(--spacing-m) sm:items-start h-full overflow-auto p-l py-xl">
             <Sidebar />
             <main className="flex flex-col gap-(--spacing-m) flex-1 pl-(--spacing-m) max-md:pl-0 pr-(--spacing-m) max-md:pr-0">
-                <ProductList searchParams={searchParams} />
+                <ProductList searchParams={params} />
             </main>
         </div>
     );
